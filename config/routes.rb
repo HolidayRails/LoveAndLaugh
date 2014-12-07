590ab6f91e8f1b59e75d4d39f1501ec98eef217d
Vinti Maheshwari <vinti.uiet@gmail.com>
2014-12-07 13:35:20 -0800

    adding zurb foundation and kid model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LoveAndLaugh::Application.routes.draw do
+  resources :kids
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
