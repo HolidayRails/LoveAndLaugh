de4931c30c3d5b28b2a17be5bbb30bdfb4195420
Vinti Maheshwari <vinti.uiet@gmail.com>
2015-03-04 12:40:26 -0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :authentications, :dependent => :destroy
-  has_many :photos
 end
