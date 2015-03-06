dd628522ec060b0fe196c9698842df7003f0e98a
Vinti Maheshwari <vinti.uiet@gmail.com>
2015-03-05 18:04:56 -0800

    added auto rotate and resizing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,8 +11,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  process :auto_orient # this should go before all other "process" steps
 
   process resize_to_fill:[800, 800]
+
+  def auto_orient
+    manipulate! do |image|
+      image.tap(&:auto_orient)
+    end
+  end
 
   # version :thumb do
   #   process :resize_to_limit => [200, 200]
