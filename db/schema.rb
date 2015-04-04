2fc8c6c1ca2dc3174ad0bb3728a3c223d3296807
Vinti Maheshwari <vinti.uiet@gmail.com>
2015-04-04 09:51:37 -0700

    added testimonials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331184905) do
+ActiveRecord::Schema.define(version: 20150403221710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,20 +47,6 @@ ActiveRecord::Schema.define(version: 20150331184905) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
-  create_table "holiays", force: true do |t|
-    t.date     "day"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "holidays", force: true do |t|
-    t.date     "day"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "messages", force: true do |t|
     t.string   "name"
     t.string   "title"
@@ -79,13 +65,20 @@ ActiveRecord::Schema.define(version: 20150331184905) do
     t.boolean  "public"
   end
 
+  create_table "testimonials", force: true do |t|
+    t.text     "entry"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
-    t.string   "email",                   default: "", null: false
-    t.string   "encrypted_password",      default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -94,8 +87,6 @@ ActiveRecord::Schema.define(version: 20150331184905) do
     t.datetime "updated_at"
     t.string   "name"
     t.boolean  "confirmed"
-    t.integer  "parent_id"
-    t.date     "feedback_requested_date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
