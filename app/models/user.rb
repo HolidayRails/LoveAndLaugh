5068666d19308a35aff4344593e2802704162429
Vinti Maheshwari <vinti.uiet@gmail.com>
2015-04-03 23:17:25 -0700

    annotated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,24 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :inet
-#  last_sign_in_ip        :inet
-#  created_at             :datetime
-#  updated_at             :datetime
-#  name                   :string(255)
-#  confirmed              :boolean
+#  id                      :integer          not null, primary key
+#  email                   :string(255)      default(""), not null
+#  encrypted_password      :string(255)      default(""), not null
+#  reset_password_token    :string(255)
+#  reset_password_sent_at  :datetime
+#  remember_created_at     :datetime
+#  sign_in_count           :integer          default(0), not null
+#  current_sign_in_at      :datetime
+#  last_sign_in_at         :datetime
+#  current_sign_in_ip      :inet
+#  last_sign_in_ip         :inet
+#  created_at              :datetime
+#  updated_at              :datetime
+#  name                    :string(255)
+#  confirmed               :boolean
+#  parent_id               :integer
+#  feedback_requested_date :date
+#
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
@@ -26,6 +29,8 @@ class User < ActiveRecord::Base
   has_many :authentications, :dependent => :destroy
   has_many :children, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_one :testimonial
+
   after_update :send_email, :if => :confirmed_changed?
 
   # def initialize
